49de8cee2695607cfaf34bdda3aec836454c4c3d
TheAvenuesSoftware <donald.X.garton@protonmail.com>
2025-05-17 07:52:39 +1000

    REM ADDED TO ALL ROWS OF _ 0 copy globalENV globalNodeMJS globalJS to this project.bat

--- _ 0 copy globalENV globalNodeMJS globalJS to this project.bat
+++ _ 0 copy globalENV globalNodeMJS globalJS to this project.bat
@@ -1,12 +1,12 @@
-robocopy C:\__TheAvenuesSoftware\__Global\utils .\src globalClient.mjs
-robocopy C:\__TheAvenuesSoftware\__Global\utils .\src globalLogin.mjs
-robocopy C:\__TheAvenuesSoftware\__Global\utils .\src globalUIpopups.mjs
-robocopy C:\__TheAvenuesSoftware\__Global\utils .\src globalSessions.mjs
-robocopy C:\__TheAvenuesSoftware\__Global\utils .\src globalNode.mjs
-robocopy C:\__TheAvenuesSoftware\__Global\utils .\src globalRoutes.mjs
-robocopy C:\__TheAvenuesSoftware\__Global\config .\config globalClient.env
-robocopy C:\__TheAvenuesSoftware\__Global\config .\config globalClientConfig.mjs
-robocopy C:\__TheAvenuesSoftware\__Global\config .\config globalNode.env
-robocopy C:\__TheAvenuesSoftware\__Global\config .\config globalNodeConfig.mjs
-robocopy C:\__TheAvenuesSoftware\__Global\styles .\styles global.css
+REM robocopy C:\__TheAvenuesSoftware\__Global\utils .\src globalClient.mjs
+REM robocopy C:\__TheAvenuesSoftware\__Global\utils .\src globalLogin.mjs
+REM robocopy C:\__TheAvenuesSoftware\__Global\utils .\src globalUIpopups.mjs
+REM robocopy C:\__TheAvenuesSoftware\__Global\utils .\src globalSessions.mjs
+REM robocopy C:\__TheAvenuesSoftware\__Global\utils .\src globalNode.mjs
+REM robocopy C:\__TheAvenuesSoftware\__Global\utils .\src globalRoutes.mjs
+REM robocopy C:\__TheAvenuesSoftware\__Global\config .\config globalClient.env
+REM robocopy C:\__TheAvenuesSoftware\__Global\config .\config globalClientConfig.mjs
+REM robocopy C:\__TheAvenuesSoftware\__Global\config .\config globalNode.env
+REM robocopy C:\__TheAvenuesSoftware\__Global\config .\config globalNodeConfig.mjs
+REM robocopy C:\__TheAvenuesSoftware\__Global\styles .\styles global.css
 pause
